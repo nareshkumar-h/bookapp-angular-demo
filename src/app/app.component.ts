7ae36c4dda21c2fdea0bc1e00d92f991e173f0b0
Naresh Kumar H <nareshkumarh@live.com>
2022-08-04 17:16:59 +0530

    Added parent to child example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent  implements OnInit{
   
-  projectName = "BookApp ";
+  projectName = "BookApp";
   isLoggedIn = false;
   
   constructor(){
